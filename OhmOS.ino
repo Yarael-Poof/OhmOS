b7cf5d3120d54075333c478cd0b54f97e41c9bcf
Mike Edwards <mikedwwb@btinternet.com>
2017-05-16 20:40:15 +0100

    made neater

--- OhmOS.ino
+++ OhmOS.ino
@@ -10,10 +10,8 @@ ACS712 sensor(ACS712_20A, A0);
 double Thermistor(int RawADC) {
  double Temp;
  Temp = log(10000.0*((1024.0/RawADC-1)));
-//         =log(10000.0/(1024.0/RawADC-1)) // for pull-up configuration
  Temp = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * Temp * Temp ))* Temp );
- Temp = Temp - 273.15;            // Convert Kelvin to Celcius
- return Temp;
+ Temp = Temp - 273.15;
 }
 
 
@@ -36,8 +34,6 @@ void setup() {
   lcd.print("Done!");
   delay(500);
   lcd.clear();
-
-
 }
 
 void loop() {
